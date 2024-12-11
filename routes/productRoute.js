6405e49acfbb3ef2826bfb90bb630e52dede82a0
Thanasilp <thanasin.pin@gmail.com>
2024-12-11 12:15:02 +0700

    deleted getbyId

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,21 +3,16 @@ import upload from "../middlewares/upload.js";
 import adminAuth from "../middlewares/adminAuth.js";
 import {
   getProduct,
-  getProductId,
   addProduct,
   removeProduct,
   updateProduct,
   singleProduct,
-  listProducts,
 } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 // Fetch all products
 productRouter.get("/", getProduct);
-
-// Fetch product by ID
-productRouter.get("/:id", getProductId);
 
 // Add a new product (requires admin authentication and image upload)
 productRouter.post(
@@ -32,9 +27,6 @@ productRouter.delete("/:id", adminAuth, removeProduct);
 
 // Fetch single product details
 productRouter.post("/single", singleProduct);
-
-// List products
-productRouter.get("/list", listProducts);
 
 // Update a product (requires admin authentication, ID passed in URL)
 productRouter.put("/:id", adminAuth, updateProduct);
